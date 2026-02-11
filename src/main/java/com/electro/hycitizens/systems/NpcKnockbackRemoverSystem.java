3d214f204d94e4d43d43b7dcf55397e7280aebda
vSKAH <vskahhh@gmail.com>
2026-02-11 23:32:05 +0100

    Fix: knockback cancellation

--- src/main/java/com/electro/hycitizens/systems/NpcKnockbackRemoverSystem.java
+++ src/main/java/com/electro/hycitizens/systems/NpcKnockbackRemoverSystem.java
@@ -1,15 +1,15 @@
 package com.electro.hycitizens.systems;
 
-import com.hypixel.hytale.component.ArchetypeChunk;
-import com.hypixel.hytale.component.CommandBuffer;
-import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.*;
 import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.entity.knockback.KnockbackComponent;
 import com.hypixel.hytale.server.core.entity.knockback.KnockbackSystems;
 import com.hypixel.hytale.server.core.modules.entity.AllLegacyLivingEntityTypesQuery;
+import com.hypixel.hytale.server.core.modules.entity.damage.DamageModule;
 import com.hypixel.hytale.server.core.modules.physics.component.Velocity;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.entities.NPCEntity;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
@@ -27,23 +27,17 @@ public class NpcKnockbackRemoverSystem extends KnockbackSystems.ApplyKnockback {
     @NonNullDecl
     @Override
     public Query<EntityStore> getQuery() {
-        return Query.and(NPCEntity.getComponentType(), KnockbackComponent.getComponentType(), Velocity.getComponentType(), Query.not(Player.getComponentType()));
+        return Query.and(KnockbackComponent.getComponentType(),NPCEntity.getComponentType());
     }
 
     @Override
     public void tick(float dt, int index, @NonNullDecl ArchetypeChunk<EntityStore> archetypeChunk, @NonNullDecl Store<EntityStore> store, @NonNullDecl CommandBuffer<EntityStore> commandBuffer) {
-        KnockbackComponent knockbackComponent = archetypeChunk.getComponent(index, KnockbackComponent.getComponentType());
-
-        assert knockbackComponent != null;
-
-        knockbackComponent.setDuration(0);
-        Velocity velocityComponent = archetypeChunk.getComponent(index, Velocity.getComponentType());
-
-        assert velocityComponent != null;
-
-        velocityComponent.getInstructions().clear();
-
         Ref<EntityStore> ref = archetypeChunk.getReferenceTo(index);
-        commandBuffer.tryRemoveComponent(ref, KnockbackComponent.getComponentType());
+        commandBuffer.removeComponent(ref, KnockbackComponent.getComponentType());
+    }
+
+    @Override
+    public SystemGroup<EntityStore> getGroup() {
+        return DamageModule.get().getFilterDamageGroup();
     }
 }
